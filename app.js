f78598a75eec4c7571b922a1093baf2b6a61180b
wchsinyi <hsinyi.wc.uma@gmail.com>
2019-04-22 13:28:19 -0700

    Implement basic React

--- app.js
+++ app.js
@@ -1,1 +1,75 @@
 // TODO
+
+
+
+
+var App2 = () => (
+  <div>Some cliche salutation2</div>
+);
+ReactDOM.render(<App2 />,
+  document.getElementById("text2")
+);
+
+var App3 = () => {
+  return <div>Some cliche salutation3</div>
+};
+ReactDOM.render(<App3 />,
+  document.getElementById("text3")
+);
+
+
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+const element = <Welcome name="Sara" />;
+ReactDOM.render(
+  element,
+  document.getElementById('text')
+);
+
+
+
+
+
+
+
+// var GroceryList = () => {
+//   return <div>
+//     <li>
+//       FreshCream
+//     </li>
+//     <li>
+//       almond
+//     </li>
+//   </div>
+// };
+// ReactDOM.render(<GroceryList />,
+//   document.getElementById("app")
+// );
+
+
+var GroceryList = () => {
+  return <div id="app_list">hiiii</div>
+};
+ReactDOM.render(<GroceryList />,
+  document.getElementById("app")
+);
+
+var Cucumbers = () => {
+  return <li>this is cucumber</li>
+};
+ReactDOM.render(<Cucumbers />,
+  document.getElementById("app_list")
+);
+
+
+
+var Kale = () => {
+  return <li>this is kale</li>
+};
+
+document.getElementById("app_list").append(Kale())
+
+
